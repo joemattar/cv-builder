192d81315f07d33fe5bc1af32a78e57ec38bdcc3
Joe M <joemattar@gmail.com>
2022-11-12 12:13:42 -0500

    Create InoutText component

---
+++ src/components/InputText/InputText.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./InputText.css";
+
+class InputText extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        id={this.props.id}
+        name={this.props.name}
+        placeholder={this.props.placeholder}
+        value={this.state.value}
+        onChange={this.handleChange}
+      ></input>
+    );
+  }
+}
+
+export default InputText;
