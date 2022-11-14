174ae8252a113c71bcf716347e0cdcae492efd0d
Joe M <joemattar@gmail.com>
2022-11-14 17:52:41 -0500

    Create ExperienceOutput component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import Header from "./components/Header/Header.js";
-import ExperienceSection from "./components/ExperienceSection/ExperienceSection";
+import ExperienceSection from "./components/ExperienceSection/ExperienceSection.js";
+import ExperienceOutput from "./components/ExperienceOutput/ExperienceOutput.js";
 import Footer from "./components/Footer/Footer.js";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="App-container">
-        <div className="inputs">
-          <ExperienceSection />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="App-container">
+          <div className="inputs">
+            <ExperienceSection />
+          </div>
+          <div className="output">
+            <ExperienceOutput />
+          </div>
         </div>
-        <div className="output"></div>
-      </div>
 
-      <Footer />
-    </div>
-  );
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ExperienceOutput/ExperienceOutput.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ExperienceOutput.css";
+
+class ExperienceOutput extends React.Component {
+  render() {
+    return (
+      <div className="experience-output">
+        <h3>WORK EXPERIENCE</h3>
+      </div>
+    );
+  }
+}
+
+export default ExperienceOutput;
