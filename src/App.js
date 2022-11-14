47c07378cd3836af21a2512161f3052c3a77252f
Joe M <joemattar@gmail.com>
2022-11-14 18:38:34 -0500

    Add item templates file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
 import ExperienceSection from "./components/ExperienceSection/ExperienceSection.js";
 import ExperienceOutput from "./components/ExperienceOutput/ExperienceOutput.js";
 import Footer from "./components/Footer/Footer.js";
-import React from "react";
+import uuid from "react-uuid";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      experienceItems: [uuid()],
+      educationItems: [uuid()],
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <div className="App-container">
           <div className="inputs">
-            <ExperienceSection />
+            <ExperienceSection items={this.experienceItems} />
           </div>
           <div className="output">
             <ExperienceOutput />
---
+++ src/templates/itemTemplates.js
@@ -0,0 +1,29 @@
+const itemTemplates = {
+  personal: {
+    firstName: "First Name",
+    lastName: "Last Name",
+    title: "Title",
+    address: "City, Province/State, Country",
+    phone: "Phone Number",
+    email: "Email Address",
+    bio: "Short Bio",
+  },
+  experience: {
+    organization: "Organization/Company Name",
+    location: "City, Country",
+    from: "From",
+    to: "To",
+    role: "Role",
+    description: "Role Description",
+  },
+  education: {
+    organization: "School/College/University Name",
+    location: "City, Country",
+    from: "From",
+    to: "To",
+    degree: "Degree",
+    field: "Field Of Study",
+  },
+};
+
+export default itemTemplates;
