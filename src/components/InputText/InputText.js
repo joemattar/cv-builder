fab059c48bcd5275a1618dbd2d9046a89c2a915e
Joe M <joemattar@gmail.com>
2022-11-17 16:15:51 -0500

    Change InputText to functional components

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,35 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InputText.css";
 
 // Define Text Input component
-class InputText extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { value: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
+function InputText(props) {
+  const [value, setValue] = useState("");
 
   // Function to handle typing, deleting in input
   // Also triggers getInputHandler function that returns
   // id and value back up to the App component
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-    this.props.getInputIdHandler(event);
+  function handleChange(event) {
+    setValue(event.target.value);
+    props.getInputIdHandler(event);
   }
 
-  render() {
-    return (
-      <input
-        type="text"
-        id={this.props.id}
-        placeholder={this.props.placeholder}
-        value={this.state.value}
-        onChange={this.handleChange}
-      ></input>
-    );
-  }
+  return (
+    <input
+      type="text"
+      id={props.id}
+      placeholder={props.placeholder}
+      value={value}
+      onChange={handleChange}
+    ></input>
+  );
 }
 
 export default InputText;
