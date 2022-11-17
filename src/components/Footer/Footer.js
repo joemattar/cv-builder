ccfa29af1b383914e99a7d67349573f6cb5300dd
Joe M <joemattar@gmail.com>
2022-11-17 16:09:34 -0500

    Change Header & Footer to functional components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,17 +2,15 @@ import React from "react";
 import "./Footer.css";
 import github_logo_image from "./github_logo_white.png";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div>THE ODIN PROJECT - JOEMATTAR</div>
-        <a href="https://github.com/joemattar" target="_blank" rel="noreferrer">
-          <img src={github_logo_image} alt="Github Logo" />
-        </a>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>THE ODIN PROJECT - JOEMATTAR</div>
+      <a href="https://github.com/joemattar" target="_blank" rel="noreferrer">
+        <img src={github_logo_image} alt="Github Logo" />
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,18 @@ import React from "react";
 import "./Header.css";
 import logo from "./logo.svg";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="logo-container">
-          <img src={logo} className="logo" alt="logo" />
-        </div>
-        <div>CV BUILDER APP</div>
-        <div className="logo-container">
-          <img src={logo} className="logo" alt="logo" />
-        </div>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <div>CV BUILDER APP</div>
+      <div className="logo-container">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
