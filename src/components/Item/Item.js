9e94e817b6b5b5108fb0fae252b62a110b4da47b
Joe M <joemattar@gmail.com>
2022-11-17 16:26:14 -0500

    Change Item & PreviewItem  to functional components

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,57 +4,49 @@ import itemTemplates from "../../templates/itemTemplates.js";
 import InputText from "../InputText/InputText.js";
 
 // Define Item component
-class Item extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.displayInputs = this.displayInputs.bind(this);
-  }
-
+function Item(props) {
   // Method to display the Input components in the Item component
-  displayInputs() {
-    const numberArray = Array(this.props.inputIDs.length)
+  function displayInputs() {
+    const numberArray = Array(props.inputIDs.length)
       .fill()
       .map((x, i) => i);
 
     const placeholderList = Object.values(
-      itemTemplates[this.props["data-type"]].placeholders
+      itemTemplates[props["data-type"]].placeholders
     );
 
     const inputList = numberArray.map((i) => (
       <InputText
-        key={this.props.inputIDs[i]}
-        id={this.props.inputIDs[i]}
+        key={props.inputIDs[i]}
+        id={props.inputIDs[i]}
         placeholder={placeholderList[i]}
-        getInputIdHandler={this.props.getInputIdHandler}
+        getInputIdHandler={props.getInputIdHandler}
       />
     ));
 
     return inputList;
   }
 
-  render() {
-    // Check if the Input component has to render buttons or not
-    let button;
-    if (itemTemplates[this.props["data-type"]].dataHasButtons === true) {
-      button = (
-        <button
-          data-type={this.props["data-type"]}
-          name={this.props.itemId}
-          onClick={this.props.deleteItemHandler}
-        >
-          Delete
-        </button>
-      );
-    }
-
-    return (
-      <div className="item" id={this.props.itemId}>
-        {this.displayInputs()}
-        {button}
-      </div>
+  // Check if the Input component has to render buttons or not
+  let button;
+  if (itemTemplates[props["data-type"]].dataHasButtons === true) {
+    button = (
+      <button
+        data-type={props["data-type"]}
+        name={props.itemId}
+        onClick={props.deleteItemHandler}
+      >
+        Delete
+      </button>
     );
   }
+
+  return (
+    <div className="item" id={props.itemId}>
+      {displayInputs()}
+      {button}
+    </div>
+  );
 }
 
 export default Item;
--- src/components/PreviewItem/PreviewItem.js
+++ src/components/PreviewItem/PreviewItem.js
@@ -3,47 +3,39 @@ import "./PreviewItem.css";
 import itemTemplates from "../../templates/itemTemplates.js";
 
 // Define PreviewItem component
-class PreviewItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.displayLabels = this.displayLabels.bind(this);
-  }
-
+function PreviewItem(props) {
   // Method to display the label elements in the Item component
-  displayLabels() {
-    const numberArray = Array(this.props.labelIDs.length)
+  function displayLabels() {
+    const numberArray = Array(props.labelIDs.length)
       .fill()
       .map((x, i) => i);
 
     const placeholderClassList = Object.values(
-      itemTemplates[this.props["data-type"]].placeholderClasses
+      itemTemplates[props["data-type"]].placeholderClasses
     );
 
     const previewLabelsList = numberArray.map((i) => (
       <label
-        key={this.props.labelIDs[i]}
-        className={`preview-label ${placeholderClassList[i]} ${this.props.labelIDs[i]}`}
+        key={props.labelIDs[i]}
+        className={`preview-label ${placeholderClassList[i]} ${props.labelIDs[i]}`}
       />
     ));
     return previewLabelsList;
   }
 
-  render() {
-    let timeSeparator;
-    if (
-      this.props["data-type"] === "experience" ||
-      this.props["data-type"] === "education"
-    ) {
-      timeSeparator = <label className="preview-label separator">|</label>;
-    }
-    return (
-      <div className="preview-item">
-        {this.displayLabels()}
-        {timeSeparator}
-      </div>
-    );
+  let timeSeparator;
+  if (
+    props["data-type"] === "experience" ||
+    props["data-type"] === "education"
+  ) {
+    timeSeparator = <label className="preview-label separator">|</label>;
   }
+  return (
+    <div className="preview-item">
+      {displayLabels()}
+      {timeSeparator}
+    </div>
+  );
 }
 
 export default PreviewItem;
